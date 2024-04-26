fa649413d0a2aabc8a295d8cc08a61fb97d2adb6
guy1228 <157746282+guy1228@users.noreply.github.com>
2024-04-26 15:02:00 -0400

    skeleton code for email notifications

---
+++ notificationHandling.js
@@ -0,0 +1,31 @@
+const nodeMailer = require('nodemailer');
+require('dotenv').config();
+
+
+const html = `
+<H1>Thank you for your order!</H1>
+<p>Your order has been placed successfully. We will notify you once your order is ready for pickup.</p>
+`
+
+async function main(){
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com', // Replace with your mail server host
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_USER, // SMTP username from environment variables
+            pass: process.env.SMTP_PASS // your SMTP password
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: 'PickupPlus <pickupplusru@gmail.com>',
+        to: 'smithg1228@gmail.com',
+        subject: 'Order Confirmation Test Message',
+        html: html
+    });
+
+    console.log("message sent:" + info.messageId)
+}
+console.log(process.env.SMTP_USER);
+main();
